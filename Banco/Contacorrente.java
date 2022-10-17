d42c18ae2c81d44f82c5687d3180a9fd8a272d48
Lucas Muniz <87603728+lucasMunizt@users.noreply.github.com>
2022-10-16 21:25:50 -0300

    Update Contacorrente.java

--- Banco/Contacorrente.java
+++ Banco/Contacorrente.java
@@ -1,31 +1,21 @@
- class Contacorrente extends Conta { // extends : herança pra herda atributos e metodos de outra classe.
+ class Contacorrente extends Conta { 
    private double limite;
-   private  boolean especial;
-   private double montateDevedor = 0;
+    double montateDevedor = 0;
     
     Contacorrente(double saldo, int numero, int agencia, Cliente cliente) {
         super(saldo, numero, agencia, cliente);
     }
        
     boolean sacar1(double valorASacar){
-            if( valorASacar <= saldo){   
-                saldo -=valorASacar;
+            if if(valorASacar >= limite) {   
+                    montanteDevedor += (saldo * (-1));
+                    saldo -=valorASacar;
                 System.out.println("seu novo saldo e: "+ saldo);
                 return true;
             }else{
-                if(especial){ // não está funcionando 
-                    if(valorASacar > limite ){
-                      //double val;
-                        saldo = ( montateDevedor + saldo + valorASacar) * (-1); 
-                       System.out.println("seu novo saldo e:sda" + saldo);
-                       return true;
-                    }else{
-                        return false;
-                    }
-                }
-            }
+                 System.out.println("Não e possivel sacar!");
+                    } 
             return false;
-
         }
             
         void depositar1(double ValorADepositar){
